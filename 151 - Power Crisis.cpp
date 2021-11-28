5b4da62e62c1108b6ef2fcdc52137e5a39136d88
linzino <linzino7@gmail.com>
2021-11-28 15:48:38 +0800

    Update 151 - Power Crisis.cpp

--- 151 - Power Crisis.cpp
+++ 151 - Power Crisis.cpp
@@ -4,6 +4,13 @@
 https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=87
 
 *******************************************************************************/
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
 
 #include <iostream>
 #include <vector>
@@ -21,13 +28,17 @@ int josphus(int n, int m){
 
 int main()
 {   
-    // -1 mean first we don't count; 
+    
     int n;  
     vector<int> regions;
-    int i;
+    int i,m;
     
     while(cin>>n && n!=0){
         
+        // -1 mean first we don't count; 
+        n=n-1;
+        
+        //build the list
         for(i = 0 ; i< n; i++)
             regions.push_back(i+1);
         
@@ -35,10 +46,11 @@ int main()
         //     cout << regions[i] << " ";
         //cout << "\n";
         
-        for(i = 1 ; i<= n; i++){
-            int end = josphus( n , i); //return index
+        for(m = 1 ; m<= n; m++){
+            int end = josphus( n , m); //return index
             if( regions[end] + 1  == 13){
-                cout << regions[end] + 1 << "\n";
+                //cout << regions[end] + 1 << "\n";  // cout lest regions
+                cout << m; // smallest m th 
                 break;
             }
         }
@@ -48,3 +60,5 @@ int main()
     
     return 0;
 }
+
+
