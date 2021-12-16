cf77079d0326899633cd70893b9d5e53d3addde2
linzino <linzino7@gmail.com>
2021-12-17 00:16:06 +0800

    Update 10405 - Longest Common.cpp

--- 10405 - Longest Common.cpp
+++ 10405 - Longest Common.cpp
@@ -8,32 +8,42 @@ using namespace std;
 
 
 int main(){
-	int i, idx;
+
 	char instr[1000];
-	vector<char> LCS;
 	
 	while(gets(instr)){
-		LCS.clear();
+	    
 		string a (instr);
 		gets(instr);
 		string b (instr);
 		
-		cout << "a :" << a << "\n";
-		cout << "b :" << b << "\n";
+// 		cout << "a :" << a << "\n";
+// 		cout << "b :" << b << "\n";
 		
-		for(i = 0; i<a.size(); i++){
-			idx = b.find(a[i]);
-			if (idx>=0){
-				LCS.push_back(a[i]);
-				b.erase(b.begin(),b.begin()+idx); 
-			}
-			
-		}
-		
-		for(i = 0; i<LCS.size();i++){
-			cout << LCS[i];
-		}
-		cout << "\nsize:" << LCS.size()<<"\n\n";
+        int L[a.size()+1][b.size()+1];
+        
+        for(int i  = 0; i< a.size()+1;i++){
+            for(int j = 0; j< b.size()+1; j++){
+                if(i == 0 || j ==0){
+                    L[i][j] = 0;
+                }else if(a[i+1]== b[j+1]){
+                    L[i][j] = L[i-1][j-1]+1;
+                }else if (L[i-1][j]> L[i][j-1]){
+                    L[i][j] = L[i-1][j];
+                }else{
+                    L[i][j] = L[i][j-1];
+                }
+            }
+        }
+        
+        // for(int i  = 0; i< a.size()+1;i++){
+        //     for(int j = 0; j< b.size()+1; j++){
+        //         cout << L[i][j]<< " ";
+        //     }
+        //     cout << "\n";
+        // }
+        
+        cout << L[a.size()][b.size()] << "\n";
 	}
 	
 	
